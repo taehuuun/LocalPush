3709c080140b6693e4350a1791ba2f6d6a4b4672
Hades000 <lsw463@naver.com>
2022-09-01 18:08:19 +0900

    Feat: maxDay에 따른 시뮬레이터 기능 구현

--- Assets/Scripts/Simulator.cs
+++ Assets/Scripts/Simulator.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class Simulator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int maxFeedDay = 1;
+    [SerializeField] private int maxCleanDay = 0;
+    [SerializeField] private int maxSizeDay = 0;
+
+    [SerializeField] private float feed = 100f;
+    [SerializeField] private float decFeed;
+    [SerializeField] private float clean = 100f;
+    [SerializeField] private float decClean;
+    [SerializeField] private float size = 20f;    
+    [SerializeField] private float incSize;
+
+    private const int DAY2MIN = 1440;
+    private WaitForSecondsRealtime delay = new WaitForSecondsRealtime(60f);
+
+    private void Start()
     {
-        
+        SettingValue();
+        StartSimulator();
     }
 
-    // Update is called once per frame
-    void Update()
+    // max일에 따른 분당 감소값 계산
+    private void SettingValue()
     {
-        
+        // dec = (퍼센트) / (맥스 데이 * 1440 (24 * 60))
+        decFeed = 100 / (maxFeedDay * DAY2MIN);
+        decClean = 100 / (maxCleanDay * DAY2MIN);
+        incSize = 80 / (maxSizeDay * DAY2MIN);
+    }
+
+    private void StartSimulator()
+    {
+        StartCoroutine(DecFood());
+        StartCoroutine(DecClean());
+        StartCoroutine(IncSize());
+    }
+
+    private IEnumerator DecFood()
+    {
+        while(feed > 0)
+        {
+            Debug.Log($"현재 FEED : {feed}");
+            yield return delay;
+            feed -= decFeed;
+        }
+    }
+
+    private IEnumerator DecClean()
+    {
+        while(clean > 0)
+        {
+            Debug.Log($"현재 CLEAN : {clean}");
+            yield return delay;
+            clean -= decClean;
+        }
+    }
+
+    private IEnumerator IncSize()
+    {
+        while(size < 100)
+        {
+            Debug.Log($"현재 SIZE : {size}");
+            yield return delay;
+            size += incSize;
+        }
     }
 }
