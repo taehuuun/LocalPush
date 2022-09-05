27385db700be4563f2f469916f355661a75ea1ea
Hades000 <lsw463@naver.com>
2022-09-06 03:11:28 +0900

    기타 프로젝트 설정 수정

--- Assets/Scripts/LocalPush.cs
+++ Assets/Scripts/LocalPush.cs
@@ -1,68 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Notifications.Android;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Unity.Notifications.Android;
 
-using NotificationServices = UnityEngine.iOS.NotificationServices;
-using NOtificationType = UnityEngine.iOS.NotificationType;
-using LocalNotification = UnityEngine.iOS.LocalNotification;
+// using NotificationServices = UnityEngine.iOS.NotificationServices;
+// using NOtificationType = UnityEngine.iOS.NotificationType;
+// using LocalNotification = UnityEngine.iOS.LocalNotification;
 
-public class LocalPush : MonoBehaviour
-{
-    [SerializeField] private Simulator simulator;
-    [SerializeField] private string channderID = "LIFE";
+// public class LocalPush : MonoBehaviour
+// {
+//     [SerializeField] private Simulator simulator;
+//     [SerializeField] private string channderID = "LIFE";
 
-    private void Start()
-    {
-        CheckNotirficationIntentData();
-        RegisterNotificationChannel();
-        AndroidNotificationCenter.OnNotificationReceived += OnNotificationReceived;
-        SendNotification();
-    }
+//     private void Start()
+//     {
+//         CheckNotirficationIntentData();
+//         RegisterNotificationChannel();
+//         AndroidNotificationCenter.OnNotificationReceived += OnNotificationReceived;
+//         SendNotification();
+//     }
 
-    private void CheckNotirficationIntentData()
-    {
-        var notificationsIntentData = AndroidNotificationCenter.GetLastNotificationIntent();
+//     private void CheckNotirficationIntentData()
+//     {
+//         var notificationsIntentData = AndroidNotificationCenter.GetLastNotificationIntent();
 
-        if(notificationsIntentData != null)
-        {
-            var id = notificationsIntentData.Id;
-            var channel = notificationsIntentData.Channel;
-            var notifications = notificationsIntentData.Notification;
-            Debug.Log($"Notification IntentData : \n ID : {id}\n Channel : {channel}\n Notifications : {notifications}");
-        }
-    }
+//         if(notificationsIntentData != null)
+//         {
+//             var id = notificationsIntentData.Id;
+//             var channel = notificationsIntentData.Channel;
+//             var notifications = notificationsIntentData.Notification;
+//             Debug.Log($"Notification IntentData : \n ID : {id}\n Channel : {channel}\n Notifications : {notifications}");
+//         }
+//     }
 
-    private void OnNotificationReceived(AndroidNotificationIntentData data)
-    {
-        Debug.Log($"Notification Received : \n ID : {data.Id}\n Channel : {data.Channel}\n Title : {data.Notification.Title}\n Text : {data.Notification.Text}");
-    }
+//     private void OnNotificationReceived(AndroidNotificationIntentData data)
+//     {
+//         Debug.Log($"Notification Received : \n ID : {data.Id}\n Channel : {data.Channel}\n Title : {data.Notification.Title}\n Text : {data.Notification.Text}");
+//     }
 
-    private void RegisterNotificationChannel()
-    {
-        var channel = new AndroidNotificationChannel()
-        {
-            Id = "LIFE",
-            Name = "Test Channel",
-            Importance = Importance.Default,
-            Description = "Generic notifications",
-        };
+//     private void RegisterNotificationChannel()
+//     {
+//         var channel = new AndroidNotificationChannel()
+//         {
+//             Id = "LIFE",
+//             Name = "Test Channel",
+//             Importance = Importance.Default,
+//             Description = "Generic notifications",
+//         };
 
-        AndroidNotificationCenter.RegisterNotificationChannel(channel);
-    }
+//         AndroidNotificationCenter.RegisterNotificationChannel(channel);
+//     }
 
-    private void SendNotification()
-    {
-        var notification = new AndroidNotification();
-        notification.Title = "생명력 10% 미만!";
-        notification.Text = "생명력이 10% 미만입니다";
-        notification.FireTime = System.DateTime.Now.AddSeconds(20);
+//     private void SendNotification()
+//     {
+//         var notification = new AndroidNotification();
+//         notification.Title = "생명력 10% 미만!";
+//         notification.Text = "생명력이 10% 미만입니다";
+//         notification.FireTime = System.DateTime.Now.AddSeconds(20);
 
-        // notification.SmallIcon = "app_icon_id";
-        // notification.LargeIcon = "app_large_icon_id";
+//         // notification.SmallIcon = "app_icon_id";
+//         // notification.LargeIcon = "app_large_icon_id";
 
-        notification.IntentData = "{\"title\":\"Notification:1\",\"data\":\"200\"}";
+//         notification.IntentData = "{\"title\":\"Notification:1\",\"data\":\"200\"}";
 
-        AndroidNotificationCenter.SendNotification(notification,channderID);
-    }
-}
+//         AndroidNotificationCenter.SendNotification(notification,channderID);
+//     }
+// }
--- Assets/Scripts/LocalPush_IOS.cs
+++ Assets/Scripts/LocalPush_IOS.cs
@@ -11,9 +11,11 @@ public class LocalPush_IOS : MonoBehaviour
         StartCoroutine(CheckRequestAuth());
     }
 
+    // iOS 알림 허용 권한 체크 코루틴
     private IEnumerator CheckRequestAuth()
     {
         var authorizationOption = AuthorizationOption.Alert | AuthorizationOption.Badge;
+
         using (var req = new AuthorizationRequest(authorizationOption, true))
         {
             while (!req.IsFinished)
@@ -29,17 +31,25 @@ public class LocalPush_IOS : MonoBehaviour
             Debug.Log(res);
         }
 
+        // 모든 작업이 끝난후 푸쉬 전송
         SendNotification();
     }
 
+    // 푸쉬 알림을 전송하는 코드
+
     private void SendNotification()
     {
+        // 딜레이를 위한 트리거 생성
         var timeTrigger = new iOSNotificationTimeIntervalTrigger()
         {
+            // 30초뒤
             TimeInterval = new TimeSpan(0, 0, 30),
+
+            // 반복하지 않음
             Repeats = false
         };
 
+        // 전송할 푸쉬 알림을 생성 및 데이터 입력
         var notification = new iOSNotification()
         {
             // You can specify a custom identifier which can be used to manage the notification later.
@@ -55,6 +65,7 @@ public class LocalPush_IOS : MonoBehaviour
             Trigger = timeTrigger,
         };
 
+        // 알림 전송 (실제 알림은 트리거의 딜레이 만큼 지난후 전송)
         iOSNotificationCenter.ScheduleNotification(notification);
     }
 }
